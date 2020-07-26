d0a64ce3c0724c7197f883ba481c4980be525270
surya talari <tejuts2000@gmail.com>
2020-07-26 14:45:25 +1000

    deleted all the node modules

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ rest_api.listen(3000)
 rest_api.use(express.static(__dirname + '/public'))
 
 // respond with "hello world" when a GET request is made to the hello page
-rest_api.get('/hello', function (req, res) {
+rest_api.get('/hello', function(req, res) {
     console.log("sending data to client")   
     res.send('hello world!!')
 })
@@ -20,24 +20,24 @@ let add_two_numbers = function(number1,number2){
 }
 
 // respond with summation of 2 numbers.
-rest_api.get('/addition',function(request, response){
+rest_api.get('/addition', function(request, response){
     let number_1 = parseFloat(request.query.number1)
     let number_2 = parseFloat(request.query.number2)
-    console.log("The first number is:",number_1)
-    console.log("The second number is:",number_2)
-    let Total = add_two_numbers(number_1,number_2)
+    console.log("The first number is:", number_1)
+    console.log("The second number is:", number_2)
+    let Total = add_two_numbers(number_1, number_2)
     console.log("sending the sum of 2 numbers",Total,"to the client.")   
     response.send('The total value after addition is:'+Total)
 })
 
 //Homework
-let hw = function(name,id){
-let accounts=[
+let hw = function(name, id){
+let accounts = [
     {id:1,name:'alex',deposit:5},
     {id:2,name:'sarah',deposit:5},
     {id:3,name:'jim',deposit:15}
     ]
-for(i=0; i<accounts.length; i++){
+for(i = 0; i < accounts.length; i += 1){
     if(name == accounts[i].name && id == accounts[i].id) {
         return accounts[i].deposit
     }
@@ -46,12 +46,12 @@ return 0
 }
 
 // respond with deposit amount of the client.
-rest_api.get('/ATM',function(request, response){
+rest_api.get('/ATM', function(request, response){
     let name = request.query.name
     let id = parseInt(request.query.id)
-    console.log("The name given by the client is:",name)
-    console.log("The id given by the client is:",id)
-    let acc_deposit = hw(name,id)
+    console.log("The name given by the client is:", name)
+    console.log("The id given by the client is:", id)
+    let acc_deposit = hw(name, id)
     if(acc_deposit == 0){
         response.send("Please verify your name and id.")
     }
